8d10388dfa8e9f765d1c5251acfa4e0b76f1219d
willywil <jonwilhelmjr@hotmail.com>
2016-06-19 18:11:37 -0500

    Adding the program

---
+++ Code Read Write/Code Read Write/Form1.Designer.cs
@@ -0,0 +1,293 @@
+﻿namespace Code_Read_Write
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.EnBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TxtOut = new System.Windows.Forms.TextBox();
+            this.CamCombo = new System.Windows.Forms.ComboBox();
+            this.EncodeINput = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SaveCombo = new System.Windows.Forms.ComboBox();
+            this.BrBtn = new System.Windows.Forms.Button();
+            this.SaBtn = new System.Windows.Forms.Button();
+            this.CodeCombo = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.StBtn = new System.Windows.Forms.Button();
+            this.DeBtn = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.CamStop = new System.Windows.Forms.Button();
+            this.xSize = new System.Windows.Forms.NumericUpDown();
+            this.ySize = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.xSize)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ySize)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 300);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // EnBtn
+            // 
+            this.EnBtn.Location = new System.Drawing.Point(318, 40);
+            this.EnBtn.Name = "EnBtn";
+            this.EnBtn.Size = new System.Drawing.Size(75, 23);
+            this.EnBtn.TabIndex = 1;
+            this.EnBtn.Text = "Encode";
+            this.EnBtn.UseVisualStyleBackColor = true;
+            this.EnBtn.Click += new System.EventHandler(this.EnBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(315, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Select Camera";
+            // 
+            // TxtOut
+            // 
+            this.TxtOut.Location = new System.Drawing.Point(318, 188);
+            this.TxtOut.Multiline = true;
+            this.TxtOut.Name = "TxtOut";
+            this.TxtOut.Size = new System.Drawing.Size(322, 126);
+            this.TxtOut.TabIndex = 3;
+            this.TxtOut.Text = "Decoded Value";
+            // 
+            // CamCombo
+            // 
+            this.CamCombo.FormattingEnabled = true;
+            this.CamCombo.Location = new System.Drawing.Point(418, 9);
+            this.CamCombo.Name = "CamCombo";
+            this.CamCombo.Size = new System.Drawing.Size(222, 21);
+            this.CamCombo.TabIndex = 4;
+            // 
+            // EncodeINput
+            // 
+            this.EncodeINput.Location = new System.Drawing.Point(399, 40);
+            this.EncodeINput.Multiline = true;
+            this.EncodeINput.Name = "EncodeINput";
+            this.EncodeINput.Size = new System.Drawing.Size(241, 23);
+            this.EncodeINput.TabIndex = 5;
+            this.EncodeINput.Text = "Enter value to encode";
+            this.EncodeINput.GotFocus += new System.EventHandler(this.EncodeINput_GotFocus);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(318, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Size";
+            // 
+            // SaveCombo
+            // 
+            this.SaveCombo.FormattingEnabled = true;
+            this.SaveCombo.Location = new System.Drawing.Point(317, 130);
+            this.SaveCombo.Name = "SaveCombo";
+            this.SaveCombo.Size = new System.Drawing.Size(241, 21);
+            this.SaveCombo.TabIndex = 9;
+            this.SaveCombo.Text = "Select Browse to Decode an Image";
+            // 
+            // BrBtn
+            // 
+            this.BrBtn.Location = new System.Drawing.Point(565, 130);
+            this.BrBtn.Name = "BrBtn";
+            this.BrBtn.Size = new System.Drawing.Size(75, 23);
+            this.BrBtn.TabIndex = 10;
+            this.BrBtn.Text = "Browse";
+            this.BrBtn.UseVisualStyleBackColor = true;
+            this.BrBtn.Click += new System.EventHandler(this.BrBtn_Click);
+            // 
+            // SaBtn
+            // 
+            this.SaBtn.Location = new System.Drawing.Point(318, 159);
+            this.SaBtn.Name = "SaBtn";
+            this.SaBtn.Size = new System.Drawing.Size(75, 23);
+            this.SaBtn.TabIndex = 11;
+            this.SaBtn.Text = "Save Code";
+            this.SaBtn.UseVisualStyleBackColor = true;
+            this.SaBtn.Click += new System.EventHandler(this.SaBtn_Click);
+            // 
+            // CodeCombo
+            // 
+            this.CodeCombo.FormattingEnabled = true;
+            this.CodeCombo.Location = new System.Drawing.Point(399, 98);
+            this.CodeCombo.Name = "CodeCombo";
+            this.CodeCombo.Size = new System.Drawing.Size(241, 21);
+            this.CodeCombo.TabIndex = 12;
+            this.CodeCombo.Text = "Select Code Type";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(319, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 16);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Code Type";
+            // 
+            // StBtn
+            // 
+            this.StBtn.Location = new System.Drawing.Point(399, 159);
+            this.StBtn.Name = "StBtn";
+            this.StBtn.Size = new System.Drawing.Size(77, 23);
+            this.StBtn.TabIndex = 14;
+            this.StBtn.Text = "Start Camera";
+            this.StBtn.UseVisualStyleBackColor = true;
+            this.StBtn.Click += new System.EventHandler(this.StBtn_Click);
+            // 
+            // DeBtn
+            // 
+            this.DeBtn.Location = new System.Drawing.Point(565, 159);
+            this.DeBtn.Name = "DeBtn";
+            this.DeBtn.Size = new System.Drawing.Size(75, 23);
+            this.DeBtn.TabIndex = 15;
+            this.DeBtn.Text = "Decode";
+            this.DeBtn.UseVisualStyleBackColor = true;
+            this.DeBtn.Click += new System.EventHandler(this.DeBtn_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // CamStop
+            // 
+            this.CamStop.Location = new System.Drawing.Point(482, 159);
+            this.CamStop.Name = "CamStop";
+            this.CamStop.Size = new System.Drawing.Size(77, 23);
+            this.CamStop.TabIndex = 16;
+            this.CamStop.Text = "Stop Camera";
+            this.CamStop.UseVisualStyleBackColor = true;
+            this.CamStop.Click += new System.EventHandler(this.CamStop_Click);
+            // 
+            // xSize
+            // 
+            this.xSize.Location = new System.Drawing.Point(364, 72);
+            this.xSize.Maximum = new decimal(new int[] {
+            300,
+            0,
+            0,
+            0});
+            this.xSize.Name = "xSize";
+            this.xSize.Size = new System.Drawing.Size(130, 20);
+            this.xSize.TabIndex = 17;
+            this.xSize.Value = new decimal(new int[] {
+            96,
+            0,
+            0,
+            0});
+            // 
+            // ySize
+            // 
+            this.ySize.Location = new System.Drawing.Point(510, 72);
+            this.ySize.Maximum = new decimal(new int[] {
+            300,
+            0,
+            0,
+            0});
+            this.ySize.Name = "ySize";
+            this.ySize.Size = new System.Drawing.Size(130, 20);
+            this.ySize.TabIndex = 18;
+            this.ySize.Value = new decimal(new int[] {
+            96,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(647, 320);
+            this.Controls.Add(this.ySize);
+            this.Controls.Add(this.xSize);
+            this.Controls.Add(this.CamStop);
+            this.Controls.Add(this.DeBtn);
+            this.Controls.Add(this.StBtn);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.CodeCombo);
+            this.Controls.Add(this.SaBtn);
+            this.Controls.Add(this.BrBtn);
+            this.Controls.Add(this.SaveCombo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.EncodeINput);
+            this.Controls.Add(this.CamCombo);
+            this.Controls.Add(this.TxtOut);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.EnBtn);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Code Read Write";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.xSize)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ySize)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button EnBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TxtOut;
+        private System.Windows.Forms.ComboBox CamCombo;
+        private System.Windows.Forms.TextBox EncodeINput;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox SaveCombo;
+        private System.Windows.Forms.Button BrBtn;
+        private System.Windows.Forms.Button SaBtn;
+        private System.Windows.Forms.ComboBox CodeCombo;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button StBtn;
+        private System.Windows.Forms.Button DeBtn;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button CamStop;
+        private System.Windows.Forms.NumericUpDown xSize;
+        private System.Windows.Forms.NumericUpDown ySize;
+    }
+}
+
---
+++ Code Read Write/Code Read Write/Form1.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MessagingToolkit.QRCode.Codec;
+using MessagingToolkit.QRCode.Codec.Data;
+using AForge;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using ZXing;
+using ZXing.QrCode;
+
+namespace Code_Read_Write
+{
+    public partial class Form1 : Form
+    {
+        private FilterInfoCollection CaptureDevice;
+        private VideoCaptureDevice FinalFrame;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                //Discovers all capture devices and adds to the Combo box
+                CaptureDevice = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+                foreach (FilterInfo Device in CaptureDevice)
+                {
+
+                    CamCombo.Items.Add(Device.Name); //Adds to Combobox
+
+                    
+
+                }
+                //if (CamCombo.Items.Count != 0)
+                //{
+                    CamCombo.SelectedIndex = 0;
+                //}
+                //else if (CamCombo.SelectedIndex == -1)
+                //{
+                    
+                //}
+            } catch
+              {
+                CamCombo.Items.Add("No camera devices available!");
+                CamCombo.SelectedIndex = 0;
+              }
+           
+            FinalFrame = new VideoCaptureDevice();
+            /*
+            string[] type = new string[20] {"AZTEC", "CODABAR", "CODE_39", "CODE_93", "CODE_128", "DATA_MATRIX", "EAN_8", "EAN_13", "ITF", "MAXICODE", "PDF_417", "QR_CODE", "RSS_14", "RSS_EXPANDED", "UPC_A", "UPC_E", "ALL_1D", "UPC_EAN_EXTENSION", "MSI", "PLESSEY" };
+            for (int i = 0; i < 20; i++)
+            {
+                CodeCombo.Items.Add(type[i]);
+            }
+             */
+            string[] type = new string[2] { "CODE_128", "QR_CODE"};
+            for (int i = 0; i < 2; i++)
+            {
+                CodeCombo.Items.Add(type[i]);
+            }
+            
+        }
+        
+        
+        private void EnBtn_Click(object sender, EventArgs e) // Button to Encode value in text field. Need to add SQL database fuctionality.
+        {
+            pictureBox1.Image = null;
+            FinalFrame.Stop();
+            timer1.Enabled = false; //Need to update once timer function is removed
+            if (EncodeINput.Text == "")
+            {
+                EncodeINput.Text = "Enter value to encode";
+            }
+            else
+            {
+                int x = Convert.ToInt32(xSize.Value);
+                int y = Convert.ToInt32(ySize.Value);
+                String code = EncodeINput.Text;
+                try
+                {
+                    switch (CodeCombo.Text)
+                    {
+                        case "CODE_128":
+                            //Default for 10 inch scan distance and 1IN x 1IN code size 1 in = 96 
+                            IBarcodeWriter enco = new BarcodeWriter { Format = BarcodeFormat.CODE_128 }; // Variable to change the format
+                            Bitmap qrpic;
+                            var result = enco.Write(code);
+                            qrpic = new Bitmap(result);
+                            qrpic = resizeImage(qrpic, new Size(x, y)); //Variable to change the size
+                            pictureBox1.Image = qrpic as Image;
+                            break;
+                        case "QR_CODE":
+                            IBarcodeWriter enco1 = new BarcodeWriter { Format = BarcodeFormat.QR_CODE }; // Variable to change the format
+                            Bitmap qrpica;
+                            var result1 = enco1.Write(code);
+                            qrpica = new Bitmap(result1);
+                            qrpica = resizeImage(qrpica, new Size(x, y)); //Variable to change the size
+                            pictureBox1.Image = qrpica as Image;
+                            break;
+                        //Need to add Default!!!!!!!!!!!!
+                    }
+                }
+                catch (Exception ex)
+                {
+                    EncodeINput.Text = "Enter value to encode";
+                }
+            }
+        }
+
+        private Bitmap resizeImage(Bitmap qrpic, System.Drawing.Size size)
+        {
+            return (Bitmap)(new Bitmap(qrpic, size));
+        }
+
+        private void BrBtn_Click(object sender, EventArgs e) //Button to Browse for file to decode
+        {
+            OpenFileDialog o = new OpenFileDialog();
+            if (o.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                pictureBox1.ImageLocation = o.FileName;
+                SaveCombo.Items.Add(o.FileName);
+                SaveCombo.Text = o.FileName;
+            }
+        }
+
+        private void DeBtn_Click(object sender, EventArgs e)
+        {
+            
+            
+            
+            //Remove timer and turn into a function that can be called by a thread and create the thread here
+            //
+            
+            
+            
+            if (pictureBox1.Image != null)
+            {
+                timer1.Enabled = true;
+            }
+        }
+
+        private void StBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                FinalFrame = new VideoCaptureDevice(CaptureDevice[CamCombo.SelectedIndex].MonikerString);
+                FinalFrame.NewFrame += new NewFrameEventHandler(FinalFrame_NewFrame);
+                pictureBox1.Visible = true;
+                FinalFrame.Start();
+                timer1.Enabled = false;
+                
+                
+                //Stop the decode thread here
+
+
+
+
+            } catch
+            {
+                MessageBox.Show("Please select a camera device!");
+            }
+            
+            
+        }
+
+        private void SaBtn_Click(object sender, EventArgs e) //Button to save image in pic box. Can I validate if pic is a code first
+        {
+            if (pictureBox1.Image != null)
+            {
+                SaveFileDialog s = new SaveFileDialog();
+                s.Filter = "PNG|*.png|JPEG|*.jpg|BMP|*.bmp|GIF|*.gif";
+                if (s.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    pictureBox1.Image.Save(s.FileName);
+                }
+            }
+        }
+
+        private void FinalFrame_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            pictureBox1.Image = (Bitmap)eventArgs.Frame.Clone();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e) //private void picDecode()
+        {
+            
+                //Every interval time this function is called
+                BarcodeReader Reader = new BarcodeReader();
+                Result result = Reader.Decode((Bitmap)pictureBox1.Image);
+                SaveFileDialog s = new SaveFileDialog();
+                s.Filter = "PNG|*.png|JPEG|*.jpg|BMP|*.bmp|GIF|*.gif";
+                int x = 0;
+                try
+                {
+                    string decoded = result.ToString().Trim(); //This can also be a SQL query
+                    TxtOut.Text = decoded;
+                    if (TxtOut.CausesValidation) //??????
+                    {
+                        
+                    }
+                }
+                catch (Exception ex) //I need to handle this better
+                {
+                    TxtOut.Text = "Code can not be read";
+                    x++;
+                    if (x == 10)
+                    {
+                        x = 0;
+                        pictureBox1.Visible = false;
+                        FinalFrame.Stop();
+                        timer1.Enabled = false;
+
+                    }
+                }
+        }
+
+        private void CamStop_Click(object sender, EventArgs e) //Button to stop the camera process
+        {
+            pictureBox1.Image = null;
+            FinalFrame.Stop();
+            timer1.Enabled = false;
+        }
+
+        private void EncodeINput_GotFocus(object sender, EventArgs e)
+        {
+            EncodeINput.Clear();
+        }
+
+    }
+}
